7ae27e57199ebe64bdc235b990abb86b2fd53243
Anton (home) <diz.vara@gmail.com>
2017-05-18 00:09:23 +0300

    transition to OpenCV HOG and SVM

--- hog_classify.py
+++ hog_classify.py
@@ -11,43 +11,43 @@ import numpy as np
 import cv2
 import glob
 import time
-from sklearn import svm
-from sklearn.svm import LinearSVC
 from sklearn.preprocessing import StandardScaler
-from skimage.feature import hog
 # NOTE: the next import is only valid for scikit-learn version <= 0.17
 # for scikit-learn >= 0.18 use:
 from sklearn.model_selection import train_test_split
 #from sklearn.cross_validation import train_test_split
 
 
-conversions = {'HSV':cv2.COLOR_RGB2HSV,
-               'HLS':cv2.COLOR_RGB2HLS,
-               'LUV':cv2.COLOR_RGB2LUV,
-               'YUV':cv2.COLOR_RGB2YUV,
-               'YCrCb':cv2.COLOR_RGB2YCrCb,
-               'LAB':cv2.COLOR_RGB2LAB,
-               'Lab':cv2.COLOR_RGB2Lab,
-               'XYZ':cv2.COLOR_RGB2XYZ
+conversions = {'HSV':cv2.COLOR_BGR2HSV,
+               'HLS':cv2.COLOR_BGR2HLS,
+               'LUV':cv2.COLOR_BGR2LUV,
+               'YUV':cv2.COLOR_BGR2YUV,
+               'YCrCb':cv2.COLOR_BGR2YCrCb,
+               'LAB':cv2.COLOR_BGR2LAB,
+               'Lab':cv2.COLOR_BGR2Lab,
+               'XYZ':cv2.COLOR_BGR2XYZ
                }
     
 
-# Define a function to return HOG features and visualization
-def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
-                        vis=False, feature_vec=True):
-    # Call with two outputs if vis==True
-    if vis == True:
-        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
-                                  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
-                                  visualise=vis, feature_vector=feature_vec)
-        return features, hog_image
-    # Otherwise call with one output
-    else:      
-        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
-                       cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
-                       visualise=vis, feature_vector=feature_vec)
-        return features
-        
+# Define a function to return HOG features
+# use hog feature from cv2
+# NB! use uint8!!!
+
+def get_hog_features(image, orient = 9, pix_per_cell = 8, cell_per_block = 2, winsize = 32):
+
+    #translate sklearn parameters into OpenCV    
+    block_size = pix_per_cell * cell_per_block;
+    block_stride = pix_per_cell;
+    
+    hog_cv = cv2.HOGDescriptor((winsize,winsize),
+                               (block_size,block_size), 
+                               (block_stride,block_stride), 
+                               (pix_per_cell,pix_per_cell), 
+                               orient)
+    
+    features = hog_cv.compute(image);
+    
+    return features
         
 
             
@@ -63,35 +63,36 @@ def extract_features(imgs, cspace='RGB', orient=9,
     # Iterate through the list of images
     for file in imgs:
         # Read in each one by one
-        image = mpimg.imread(file)
-        # apply color conversion if other than 'RGB'
-        image = cv2.resize(image, (32,32))
-        if (cspace in conversions.keys()):        
-            feature_image = cv2.cvtColor(image, conversions[cspace])
-        else:
-            feature_image = np.copy(image)        
-
-        # Call get_hog_features() with vis=False, feature_vec=True
-        if type(hog_channel) == list or type(hog_channel) == tuple:
-            hog_features = []
-            for channel in hog_channel:
-                hog_features.append(get_hog_features(feature_image[:,:,channel], 
-                                    orient, pix_per_cell, cell_per_block, 
-                                    vis=False, feature_vec=True))
-            hog_features = np.ravel(hog_features)        
-        elif hog_channel == 'ALL':
-            hog_features = []
-            for channel in range(feature_image.shape[2]):
-                hog_features.append(get_hog_features(feature_image[:,:,channel], 
-                                    orient, pix_per_cell, cell_per_block, 
-                                    vis=False, feature_vec=True))
-            hog_features = np.ravel(hog_features)        
-        else:
-            hog_features = get_hog_features(feature_image[:,:,hog_channel], orient, 
-                        pix_per_cell, cell_per_block, 
-                        vis=False, feature_vec=True)
-        # Append the new feature vector to the features list
-        features.append(hog_features)
+        image = cv2.imread(file)
+        if (image != None and image.size > 0):
+            # apply color conversion if other than 'RGB'
+            image = cv2.resize(image, (32,32))
+            if (cspace in conversions.keys()):        
+                feature_image = cv2.cvtColor(image, conversions[cspace])
+            else:
+                feature_image = np.copy(image)        
+    
+            # Call get_hog_features() with vis=False, feature_vec=True
+            if type(hog_channel) == list or type(hog_channel) == tuple:
+                hog_features = []
+                for channel in hog_channel:
+                    hog_features.append(get_hog_features(feature_image[:,:,channel], 
+                                        orient, pix_per_cell, cell_per_block, 32));
+                                        
+                hog_features = np.ravel(hog_features)        
+            elif hog_channel == 'ALL':
+                hog_features = []
+                for channel in range(feature_image.shape[2]):
+                    hog_features.append(get_hog_features(feature_image[:,:,channel], 
+                                        orient, pix_per_cell, cell_per_block, 32)); 
+                                        
+                hog_features = np.ravel(hog_features)        
+            else:
+                hog_features = get_hog_features(feature_image[:,:,hog_channel], orient, 
+                            pix_per_cell, cell_per_block, 32); 
+                            
+            # Append the new feature vector to the features list
+            features.append(hog_features)
     # Return list of feature vectors
     return features
 
@@ -134,10 +135,11 @@ X = np.vstack((car_features, notcar_features)).astype(np.float64)
 # Fit a per-column scaler
 X_scaler = StandardScaler().fit(X)
 # Apply the scaler to X
-scaled_X = X_scaler.transform(X)
+scaled_X = X_scaler.transform(X).astype(np.float32)
 
 # Define the labels vector
-y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+y = np.hstack((np.ones(len(car_features)), 
+               np.zeros(len(notcar_features)))).astype(np.int32)
 
 
 # Split up data into randomized training and test sets
@@ -149,17 +151,11 @@ print('Using:',orient,'orientations',pix_per_cell,
     'pixels per cell and', cell_per_block,'cells per block')
 print('Feature vector length:', len(X_train[0]))
 # Use a linear SVC 
-svc = svm.SVC(C=2.3, kernel = 'rbf', gamma = 6.5e-4)
+
 
 #%%             
 # Check the training time for the SVC
-t=time.time()
-svc.fit(X_train, y_train)
-t2 = time.time()
-print(round(t2-t, 2), 'Seconds to train SVC...')
-# Check the score of the SVC
-print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
-
+uSvm = cv_svm (X_train, X_test, y_train, y_test)
 #%%
 # Check the prediction time for a single sample
 
@@ -168,29 +164,12 @@ np.random.shuffle(idx)
 
 t=time.time()
 n_predict = 1000
-yr = svc.predict(X_test[idx[0:n_predict]])
+yr = uSvm.predict(X_test[idx[0:n_predict]])[1].ravel()
 print(sum( yr != y_test[idx[0:n_predict]]), " mistakes from ", n_predict)
 #print('For these',n_predict, 'labels: ', y_test[0:n_predict])
 t2 = time.time()
 print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
 
-#%%
-
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import RandomizedSearchCV
-do_fit = False;
-
-if (do_fit):
-    parameters = {'C': np.arange(2.2, 2.4, .05), 'gamma': np.arange(6.4e-4, 6.6e-4, 1e-5)}
-    svr = svm.SVC()
-    clf = GridSearchCV(svr, parameters)
-    np.random.shuffle(idx)
-    X_tr = X_train[idx[:1000]]
-    y_tr = y_train[idx[:1000]]
-    
-    clf.fit(X_tr, y_tr)
-    
-    print(clf.best_params_, clf.best_score_)
 
 #%%
 t=time.time()
@@ -207,10 +186,11 @@ myX = np.vstack((myCarFeatures, myNotCarFeatures)).astype(np.float64)
 # Fit a per-column scaler
 X_scaler = StandardScaler().fit(myX)
 # Apply the scaler to X
-scaled_X = X_scaler.transform(myX)
+scaled_X = X_scaler.transform(myX).astype(np.float32)
 
 # Define the labels vector
-myY = np.hstack((np.ones(len(myCarFeatures)), np.zeros(len(myNotCarFeatures))))
+myY = np.hstack((np.ones(len(myCarFeatures)), 
+                 np.zeros(len(myNotCarFeatures)))).astype(np.int32)
 
 
 # Split up data into randomized training and test sets
@@ -218,19 +198,13 @@ rand_state = np.random.randint(0, 100)
 myX_train, myX_test, myY_train, myY_test = train_test_split(
     scaled_X, myY, test_size=0.2, random_state=rand_state)
 #%%
-mySvc = svm.SVC(C=2.3, kernel = 'rbf', gamma = 6.5e-4)
+mySvm = cv_svm (myX_train, myX_test, myY_train, myY_test)
 
-           
-# Check the training time for the SVC
-t=time.time()
-mySvc.fit(myX_train, myY_train)
-t2 = time.time()
-print(round(t2-t, 2), 'Seconds to train SVC...')
 # Check the score of the SVC
-print('Test Accuracy of mySVC on my data = ', round(mySvc.score(myX_test, myY_test), 4))
-print('Test Accuracy of uSVC on my data  = ', round(svc.score(myX_test, myY_test), 4))
-print('Test Accuracy of mySVC on u data = ', round(mySvc.score(X_test, y_test), 4))
-print('Test Accuracy of uSVC on u data  = ', round(svc.score(X_test, y_test), 4))
+print('Test Accuracy of mySVC on my data = ', round(score(mySvm,myX_test, myY_test), 4))
+print('Test Accuracy of uSVC on my data  = ', round(score(uSvm,myX_test, myY_test), 4))
+print('Test Accuracy of mySVC on u data = ', round(score(mySvm,X_test, y_test), 4))
+print('Test Accuracy of uSVC on u data  = ', round(score(uSvm,X_test, y_test), 4))
 
 #%%
 idx = np.arange(len(myY_test))
@@ -238,17 +212,17 @@ np.random.shuffle(idx)
 
 t=time.time()
 n_predict = 1000
-yr = svc.predict(myX_test[idx[0:n_predict]])
+yr = uSvm.predict(myX_test[idx[0:n_predict]])[1].ravel()
 #print('For these',n_predict, 'labels: ', y_test[0:n_predict])
 t2 = time.time()
-print(sum( yr != myY_test[idx[0:n_predict]]), " mistakes from ", n_predict)
+print("u on my",sum( yr != myY_test[idx[0:n_predict]]), " mistakes from ", n_predict)
 print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
 
 t=time.time()
-yr = mySvc.predict(myX_test[idx[0:n_predict]])
+yr = mySvm.predict(myX_test[idx[0:n_predict]])[1].ravel()
 #print('For these',n_predict, 'labels: ', y_test[0:n_predict])
 t2 = time.time()
-print(sum( yr != myY_test[idx[0:n_predict]]), " mistakes from ", n_predict)
+print("my on my",sum( yr != myY_test[idx[0:n_predict]]), " mistakes from ", n_predict)
 print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
 
 
